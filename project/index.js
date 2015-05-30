8bba5c57acf09b5dcc30e12d2fc762dc5f67cb91
Richard King <richrdkng@gmail.com>
2015-05-30 19:39:55 +0200

    Update main project file

--- project/index.js
+++ project/index.js
@@ -1,50 +1,86 @@
 'use strict';
 
     // Project details
-var name        = 'eachof',
-    description = 'A function to iterate through arraylikes and objects and check if contains the given parameter',
-    version     = '1.0.0',
-    homepage    = 'http://richrdkng.github.io/eachof/',
+var name    = 'eachof',
+    version = '1.0.0',
+    license = 'MIT',
+
+    description  = 'jQuery\'s .hide() extension, that adds an additional option to remove the element after it is hidden.',
+    homepage     = 'https://github.com/richrdkng/eachof',
+    authors      = [
+        ['Richard King', 'richrdkng@gmail.com', 'https://github.com/richrdkng']
+    ],
+
+    main    = 'index.js',
+    scripts = {
+        gulp:  'gulp --gulpfile task/index.js',
+        build: 'npm install && grunt build',
+        test:  'grunt --verbose'
+    },
+
     repository = {
         credentials: null,
         git: 'https://github.com/richrdkng/eachof.git'
     },
-    issues = '',
+    issues = 'https://github.com/richrdkng/eachof/issues',
 
-    authors = [
-        ['Richard King', 'richrdkng@gmail.com', 'https://github.com/richrdkng']
-    ],
-    license = [
-        'MIT'
-    ],
-    keywords = [],
     dependencies = {
-        dependencies: {},
-        peerDependencies: {},
-        devDependencies: {}
+    },
+    devDependencies = {
     },
 
-    // Paths
-    p = require('path'),
-    root = p.dirname(__dirname),
-    path = {
-        root:       root,
-        master:     root,
-        ghpages:    root,
-        project:    p.normalize(root+'/project'),
-        image:      p.normalize(root+'/image'),
-        markup:     p.normalize(root+'/markup'),
-        script:     p.normalize(root+'/script'),
-        style:      p.normalize(root+'/style')
-    },
+    keywords = [
+        'jquery',
+        'exists',
+        'plugin',
+        'jquery-plugin',
+        'jquery-exists',
+        'jquery.exists',
+        'javascript',
+        'js',
+        'amd',
+        'requirejs',
+        'production',
+        'tool'
+    ],
 
-    log = console.log;
+    path = require('path'),
+    root = path.dirname(__dirname);
 
+    // include credentials, if present
+    try {
+        repository.credentials = require('./credentials');
+    } catch(error) {
+        repository.credentials = false;
+    }
 
 module.exports = {
-    name:           name,
-    description:    description,
-    version:        version,
-    path:           path,
-    repository:     repository
+    name:            name,
+    version:         version,
+    license:         license,
+
+    description:     description,
+    homepage:        homepage,
+    authors:         authors,
+
+    main:            main,
+    scripts:         scripts,
+
+    repository:      repository,
+    issues:          issues,
+
+    dependencies:    dependencies,
+    devDependencies: devDependencies,
+    keywords:        keywords,
+
+    primary:         path.normalize(root + '/src/eachof.js'),
+    path:            {
+        root:        root,
+        master:      root,
+        ghpages:     path.normalize(root + '/../gh-pages'),
+        project:     path.normalize(root + '/project'),
+        src:         path.normalize(root + '/src'),
+        test:        path.normalize(root + '/test'),
+        dist:        path.normalize(root + '/dist')
+    }
 };
