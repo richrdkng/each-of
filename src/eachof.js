3c290a6489ea52d1c1b3443a330b62a944b01ef0
Richard King <richrdkng@gmail.com>
2015-06-09 20:43:49 +0200

    Restructure to be more concise

--- src/eachof.js
+++ src/eachof.js
@@ -49,6 +49,117 @@ var log = console.log.bind(console);
 
         // If it is a collection
         if (collection && typeof collection === 'object') {
+            var isArrayLike,
+                element;
+
+            if (collection.constructor === Array) {
+                isArrayLike = true;
+                //log('isArrayLike');
+
+            } else if (collection.constructor === Object) {
+                isArrayLike = true;
+
+                var tempCollection = [];
+                length = 0;
+
+                for (element in collection) {
+                    if (collection.hasOwnProperty(element)) {
+                        tempCollection[length++] = collection[element];
+                    }
+                }
+
+                collection = tempCollection;
+
+            } else {
+                switch(collection.constructor) {
+                    case Int8Array:
+                    case Uint8Array:
+                    case Uint8ClampedArray:
+                    case Int16Array:
+                    case Uint16Array:
+                    case Int32Array:
+                    case Uint32Array:
+                    case Float32Array:
+                    case Float64Array:
+                        isArrayLike = true;
+                    default:
+                        isArrayLike = Object.prototype.toString.call(collection) === '[object Arguments]';
+                }
+            }
+
+            if (!isArrayLike) {
+                return false;
+            }
+
+            //log('it is array-like');
+            length = collection.length;
+
+            if (length === 0) {
+                //log("if (length === 0) {");
+                return false;
+            }
+
+            matches = true;
+
+            for (var i = 0; i < length; ++i) {
+                element = collection[i];
+
+                // Check, whether the element is NOT NaN, as if the element is equal to itself, it is not NaN
+                if (element === element) {
+                    if (!conditionIsNaN) {
+                        if (element !== condition) {
+                            matches = false;
+                            break;
+                        }
+                    } else {
+                        matches = false;
+                        break;
+                    }
+
+                // If the element is NaN
+                } else {
+                    if (!conditionIsNaN) {
+                        matches = false;
+                        break;
+                    }
+                }
+            }
+
+            //log('matches:', matches);
+
+            return matches;
+
+        }
+
+        // If it is not a collection, then handle it as if it were a strict equality check
+        return collection === condition;
+    }
+
+    /*
+    function eachof(collection, condition) {
+        var matches = false,
+            length = 0;
+
+        // When no condition was given or called without parameters, return false
+        if (arguments.length < 2) {
+            return false;
+        }
+
+        var collectionIsNaN = collection !== collection,
+            conditionIsNaN  = condition !== condition;
+
+        // When the collection is NaN, but the condition isn't, then return false
+        if (collectionIsNaN && !collectionIsNaN) {
+            return false;
+        }
+
+        // When the collection and the condition are both NaNs, as they are certainly equal, return true
+        if (collectionIsNaN && conditionIsNaN) {
+            return true;
+        }
+
+        // If it is a collection
+        if (collection && typeof collection === 'object') {
             var isArrayLike;
 
             if (collection.constructor === Array) {
@@ -168,6 +279,7 @@ var log = console.log.bind(console);
 
         return matches;
     };
+    */
 
     module.exports = eachof;
 
