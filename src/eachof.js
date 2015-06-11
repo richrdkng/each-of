10ad318854c0176af4c8cab5197afb34f3ee22b6
Richard King <richrdkng@gmail.com>
2015-06-11 06:10:07 +0200

    Add isEqual function to conform to DRY principles

--- src/eachof.js
+++ src/eachof.js
@@ -16,6 +16,7 @@
 
 // TODO: IE8 compatibility
 // TODO: strict vs regular equality
+// TODO: recursive checking
 // TODO: function as a condition
 // TODO: multiple conditions / array condition / object condition / array-like condition
 // TODO: UMD
@@ -58,104 +59,77 @@ var log = console.log.bind(console);
 
         if (collection.constructor === Object) {
             var element,
-                length         = 0,
-                tempCollection = [];
+                length = 0,
+                temp   = [];
 
                 for (element in collection) {
                     if (collection.hasOwnProperty(element)) {
-                        tempCollection[length++] = collection[element];
+                        temp[length++] = collection[element];
                     }
                 }
 
-                collection = tempCollection;
+                collection = temp;
         }
 
         return collection;
     }
 
+    function isEqual(element, condition) {
+        var elementIsNaN   = element !== element,
+            conditionIsNaN = condition !== condition;
+
+        // When the element is NaN, but the condition isn't, then return false
+        if (elementIsNaN && !conditionIsNaN) {
+            return false;
+        }
+
+        // When the collection and the condition are both NaNs, as they are certainly equal, return true
+        if (elementIsNaN && conditionIsNaN) {
+            return true;
+        }
+
+        // If the are strictly equal, return true
+        if (element === condition) {
+            return true;
+        }
+
+        if (isCollection(condition)) {
+            for (var i = 0, length = condition.length; i < length; ++i) {
+                if (element === condition[i]) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
     function eachof(collection, condition) {
-        var conditionIsCollection;
 
         // When no condition was given or called without parameters, return false
         if (arguments.length < 2) {
             return false;
         }
 
-        var collectionIsNaN = collection !== collection,
-            conditionIsNaN  = condition !== condition;
-
-        // When the collection is NaN, but the condition isn't, then return false
-        if (collectionIsNaN && !collectionIsNaN) {
-            return false;
-        }
-
-        // When the collection and the condition are both NaNs, as they are certainly equal, return true
-        if (collectionIsNaN && conditionIsNaN) {
-            return true;
-        }
-
-        if (isCollection(condition)) {
-            conditionIsCollection = true;
-
-            condition = asCollection(condition);
-            if (condition.length === 0) {
-                return false;
-            }
-        }
-
         // If it is a collection
         if (isCollection(collection)) {
-
             collection = asCollection(collection);
 
             if (collection.length === 0) {
                 return false;
             }
 
-            for (var i = 0, length = collection.length, element; i < length; ++i) {
-                element = collection[i];
-
-                // Check, whether the element is NOT NaN, as if the element is equal to itself, it is not NaN
-                if (element === element) {
-                    if (!conditionIsNaN) {
-                        // If the condition is non-collection
-                        if (!conditionIsCollection) {
-                            if (element !== condition) {
-                                return false;
-                            }
-                        } else {
-                            for (var j = 0, len = condition.length, el; j < len; ++j) {
-                                el = condition[j];
-
-                                // If whether it is not NaN
-                                if (el === el) {
-                                    if (element !== el) {
-                                        return false;
-                                    }
-
-                                // If the current element of the condition is NaN, but the current element of the
-                                } else {
-                                    return false;
-                                }
-                            }
-                        }
-                    } else {
-                        return false;
-                    }
-
-                // Otherwise the element is NaN
-                } else {
-                    if (!conditionIsNaN) {
-                        return false;
-                    }
+            for (var i = 0, length = collection.length; i < length; ++i) {
+                if (!isEqual(collection[i], condition)) {
+                    return false;
                 }
             }
 
             return true;
         }
 
-        // Otherwise it is not a collection, then handle it as if it were a strict equality check
-        return collection === condition;
+        // Otherwise it is not a collection
+        return isEqual(collection, condition);
     }
 
     module.exports = eachof;
